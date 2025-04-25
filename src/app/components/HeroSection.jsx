7f051cdff2dc8ea254435bad327f84814b569cbd
amshugannu <120478234+amshugannu@users.noreply.github.com>
2025-04-25 19:07:33 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -63,13 +63,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[420px] lg:h-[420px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[420px] lg:h-[420px] relative shadow-[0_0_25px_#3B82F6] overflow-hidden">
             <Image
               src="/images/profile.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={350}
-              height={250}
+              fill
+              className="rounded-full object-cover object-top"
+              priority
             />
           </div>
         </motion.div>
